6d32e1f628c892c56e30c7a515bffe30eee467e3
cdugo <54993517+cdugo@users.noreply.github.com>
2019-10-08 11:26:04 -0400

    as

---
+++ app.js
@@ -0,0 +1,41 @@
+class Sneaker {
+    constructor(name, color, releaseDate, retailPrice, profit){
+        this.age = 18;
+        this.name = 'Carlo';
+    }
+    calculateBirthYear(){
+        return (new Date().getFullYear - this.age);
+    };
+};
+
+const steve = new Person (44, 'Steve');
+ 
+
+//one class to create the object person, album, movie ***Model
+//take args
+
+
+//****View */ class for the UI which contains methods
+// creates/insert html for the UI DISPLAY
+//clear fields method
+//remove something/object
+
+
+
+
+//controller combines the ui and model
+//event handler with function
+//get values
+
+
+
+//instatiate new class/object with variable name
+
+
+//instantiate new UI const ui = new UI()
+
+
+
+//call relevant methods
+
+//separate event handler for removing/deleting
