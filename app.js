2be07452e1df17f06919daa30a31dcf341a63c8a
cdugo <54993517+cdugo@users.noreply.github.com>
2019-10-11 11:49:38 -0400

    as

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-/*  class Sneaker {
-    constructor(name, color, releaseDate, retailPrice, profit){
-        this.styleCode = this.styleCode
-        this.name = 'Carlo';
-    }
-    calculateBirthYear(){
-        return (new Date().getFullYear - this.age);
-    };
-};  */
-var request = 'request'
-request.get('https://stockx.com/api/v3/browse?productCategory=sneakers', function(error, response, body){
-    console.log('body',body);
-})
-
-
 //one class to create the object person, album, movie ***Model
 //take args
 
+class Sneaker {
+    constructor(name, size, price, sold, image){
+        this.name = name;
+        this.size = size;
+        this.price = price;
+        this.image = image;
+        this.sold = sold;
+    }
+    calculateProfit(){
+        return (this.sold - this.price);
+    };
+};  
+
+const jordan = new Sneaker('jrodan', 11, 240, 'no', 260);
+console.log(jordan);
+
+const userName = document.querySelector('#name');
+const userSize = document.querySelector('#size');
+const userPrice = document.querySelector('#price');
+const userImage = document.querySelector('#image');
+const display = document.querySelector('.display');
 
 //****View */ class for the UI which contains methods
 // creates/insert html for the UI DISPLAY
 //clear fields method
 //remove something/object
+class ui{
+    htmlAddition(){
+        let html = '<div class="display-sneaker"><div class="display-name">%name%</div><div class="display-size">%size%</div><div class="display-price">%price%</div><div class="display-sold">%sold%</div><div class="display-image"><img src="%url%" alt=""></div><div class="remove-sneaker"><p class="remove-sneaker"> Remove Sneaker &#10006;</p></div></div>'; 
+
+    let newHtml = html.replace('%name%', userName.value);
+    newHtml = newHtml.replace('%size%', userSize.value);
+    newHtml = newHtml.replace('%price%', userPrice);
+    newHtml = newHtml.replace('%sold%', userSold)
+    }
+
+}
 
 
 
