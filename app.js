cdb4b8ccf49234cb6057ef4b16d8784032fd3c92
cdugo <54993517+cdugo@users.noreply.github.com>
2019-10-15 11:47:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ class Sneaker {
         this.price = price;
         this.image = image;
         this.sold = sold;
+        
     }
     calculateProfit(){
         return (this.sold - this.price);
     };
 };  
 
-const jordan = new Sneaker('jrodan', 11, 240, 'no', 260);
-console.log(jordan);
-
+/* const jordan = new Sneaker('jrodan', 11, 240, 'no', 260);
+console.log(jordan); */
+const form = document.querySelector('#sneaker-form')
 const userName = document.querySelector('#name');
 const userSize = document.querySelector('#size');
 const userPrice = document.querySelector('#price');
@@ -26,23 +27,49 @@ const display = document.querySelector('.display');
 //****View */ class for the UI which contains methods
 // creates/insert html for the UI DISPLAY
 //clear fields method
+
+
 //remove something/object
+
+
+
 class ui{
     htmlAddition(){
         let html = '<div class="display-sneaker"><div class="display-name">%name%</div><div class="display-size">%size%</div><div class="display-price">%price%</div><div class="display-sold">%sold%</div><div class="display-image"><img src="%url%" alt=""></div><div class="remove-sneaker"><p class="remove-sneaker"> Remove Sneaker &#10006;</p></div></div>'; 
 
     let newHtml = html.replace('%name%', userName.value);
     newHtml = newHtml.replace('%size%', userSize.value);
-    newHtml = newHtml.replace('%price%', userPrice);
-    newHtml = newHtml.replace('%sold%', userSold)
+    newHtml = newHtml.replace('%price%', userPrice.value);
+    newHtml = newHtml.replace('%sold%', userSold.value);
+    newHtml = newHtml.replace('%url%', userImage.value);
+    document.querySelector('.display').insertAdjacentHTML('beforeend', newHtml);
     }
+    clearFields(){
+        document.getElementById('name');
+        document.getElementById('size');
+        document.getElementById('price');
+        document.getElementById('image');
+    };
+    
+    removeSneaker(el){
+        if(el.target.parentElement.classList.contains('remove-sneaker')){
+            el.target.parentElement.parentElement.remove();
+        };
 
-}
+};
 
 
 
-
+document.getElementbyId('sneaker-form').addEventListener('submit',function(e)){
+    const form = document.querySelector('#sneaker-form')
+    const userName = document.querySelector('#name');
+    const userSize = document.querySelector('#size');
+    const userPrice = document.querySelector('#price');
+    const userImage = document.querySelector('#image');
+    const display = document.querySelector('.display');
+};
 //controller combines the ui and model
+
 //event handler with function
 //get values
 
@@ -57,4 +84,4 @@ class ui{
 
 //call relevant methods
 
-//separate event handler for removing/deleting
+//separate event handler for removing/deleting 
